a873432a7448c8bb3bcbdd80fc51788a1be06d65
Poe_2332 <40632015@live.napier.ac.uk>
2024-02-02 18:31:54 +0630

    Updated sort function

--- src/main/java/DevOps_Team8/CountryLoader.java
+++ src/main/java/DevOps_Team8/CountryLoader.java
@@ -4,19 +4,11 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Load country data from world database
- */
 public class CountryLoader {
-    // Connect to world.sql server
     private static final String JDBC_URL = "jdbc:mysql://db:3306/world?useSSL=false";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "example";
 
-    /**
-     * Load each country data
-     * @return list of country object with data
-     */
     public List<Country> loadCountryData() {
         List<Country> countries = new ArrayList<>();
 
--- src/main/java/DevOps_Team8/CountryReport.java
+++ src/main/java/DevOps_Team8/CountryReport.java
@@ -1,34 +1,54 @@
 package DevOps_Team8;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 
-/**
- * Functions for Country Report: Get country data from database and display data
- */
 public class CountryReport {
+    //Declare private variable
     private final List<Country> countries;
 
 
     /**
-     * Contract country data object
-     * @param countries
+     *Construct method for country report class
      */
     public CountryReport(List<Country> countries) {
         this.countries = countries;
     }
 
     /**
-     * Sort all country around the world by population descending and display data
+     * Sort countries by population in descending order and print them
      */
     public void sortByPopulation() {
-        countries.sort(Comparator.comparingInt(Country::getPopulation).reversed());
-        System.out.println("Countries sorted by population:");
-        System.out.printf("%-5s %-50s %-20s %-25s %-20s %-20s%n", "Code", "Name", "Continent", "Region", "Population", "Capital");
-        for (Country country : countries) {
-            System.out.printf("%-5s %-50s %-20s %-25s %-20s %-20s%n",
-                    country.getCode(), country.getName(), country.getContinent(),
-                    country.getRegion(), country.getPopulation(), country.getCapital());
-        }
+        sortByPopulationAndPrint("Countries sorted by population:", countries);
     }
+
+
+    /**
+     * Sort the countries list by descending order and display them with a message
+     *
+     * @param message The message will be displayed before countries list
+     * @param countries List of countries to sort
+     */
+    private void sortByPopulationAndPrint(String message, List<Country> countries) {
+        countries.sort(Comparator.comparingInt(Country::getPopulation).reversed());
+        printCountries(message, countries);
+    }
+
+
+
+    /**
+     * Print list of countries
+     * @param message The message will be displayed before countries list
+     * @param countries List of countries to display
+     */
+    private void printCountries(String message, List<Country> countries) {
+        System.out.println(message);
+        System.out.printf("%-5s %-50s %-20s %-25s %-20s %-20s%n", "Code", "Name", "Continent", "Region", "Population", "Capital");
+        countries.forEach(country -> System.out.printf("%-5s %-50s %-20s %-25s %-20s %-20s%n",
+                country.getCode(), country.getName(), country.getContinent(),
+                country.getRegion(), country.getPopulation(), country.getCapital()));
+        System.out.println(); // Add blank line for separation
+    }
+
+
 }
