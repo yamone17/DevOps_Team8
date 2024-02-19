6149598e6dc4739b882d42481f9310505e8da758
Poe_2332 <40632015@live.napier.ac.uk>
2024-02-19 12:54:41 +0630

    Added my sql testing

--- src/test/java/DevOps_Team8/AppTest.java
+++ src/test/java/DevOps_Team8/AppTest.java
@@ -1,6 +1,7 @@
 package DevOps_Team8;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.sql.*;
 import java.util.ArrayList;
@@ -10,19 +11,25 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+
 /**
  * Unit testing for function is it work perfect or not
  */
 public class AppTest {
 
     static CountryReport countryReport;
+    static CityReport cityReport;
+    private MySQLSetUp mySQL;
+    private Connection connection;
+    static CapitalReport capitalReport;
+    static PopulationReport populationReport;
 
     /**
      * Create arraylist and add arraylist for testing
      */
-    @BeforeAll
-    static void init() {
-        // Assuming you have a method to fetch country data
+    @BeforeEach
+    void setCountryReport() {
+        // Assuming to fetch country data
         ArrayList<Country> countries = new ArrayList<>();
         countries.add(new Country("USA", "United States", 278357000, "North America", "North America", "Washington"));
         countries.add(new Country("IND", "India", 1013662000, "Asia", "Southern and Central Asia", "New Delhi"));
@@ -65,13 +72,120 @@ public class AppTest {
         assertEquals("Washington", country.getCapital());
     }
 
+
+    @BeforeEach
+    void setCityReport() {
+        // Assuming to fetch city data
+        ArrayList<City> cities = new ArrayList<City>();
+        cities.add(new City("Washington", "United States", 572059, "District of Columbia", "North America", "North America"));
+        cities.add(new City("New Delhi", "India", 301297, "Delhi", "Asia", "Southern and Central Asia"));
+        cities.add(new City("Peking", "China", 7472000, "Peking", "Asia", "Eastern Asia"));
+        cityReport = new CityReport(cities);
+    }
+
+    @Test
+    public void testLoadCityData() throws SQLException {
+        // Mocking database connection and result set
+        Connection mockConnection = mock(Connection.class);
+        Statement mockStatement = mock(Statement.class);
+        ResultSet mockResultSet = mock(ResultSet.class);
+
+        // Mocking query result
+        when(mockConnection.createStatement()).thenReturn(mockStatement);
+        when(mockStatement.executeQuery(anyString())).thenReturn(mockResultSet);
+        when(mockResultSet.next()).thenReturn(true, false);
+        when(mockResultSet.getString("CityName")).thenReturn("Washington");
+        when(mockResultSet.getString("CountryName")).thenReturn("United States");
+        when(mockResultSet.getInt("CityPopulation")).thenReturn(572059);
+        when(mockResultSet.getString("Continent")).thenReturn("North America");
+        when(mockResultSet.getString("Region")).thenReturn("North America");
+        when(mockResultSet.getString("District")).thenReturn("District of Columbia");
+
+        // Create the CountryLoader instance
+        CityLoader cityLoader = new CityLoader();
+
+        // Call the method to be tested
+        List<City> cities = cityLoader.loadCityData(mockConnection);
+
+        // Verify the result
+        assertEquals(1, cities.size());
+        City city = cities.get(0);
+        assertEquals("Washington", city.getName());
+        assertEquals("United States", city.getCountry());
+        assertEquals(572059, city.getPopulation());
+        assertEquals("North America", city.getContinent());
+        assertEquals("North America", city.getRegion());
+        assertEquals("District of Columbia", city.getDistrict());
+    }
+
+    @BeforeEach
+    void setCapitalReport() {
+        // Assuming to fetch capital data
+        ArrayList<Capital> capitals = new ArrayList<Capital>();
+        capitals.add(new Capital("Washington", "United States", 572059, "North America", "North America"));
+        capitals.add(new Capital("New Delhi", "India", 301297, "Asia", "Southern and Central Asia"));
+        capitals.add(new Capital("Peking", "China", 7472000, "Asia", "Eastern Asia"));
+        capitalReport = new CapitalReport(capitals);
+    }
+
+    @Test
+    public void testLoadCapitalData() throws SQLException {
+        // Mocking database connection and result set
+        Connection mockConnection = mock(Connection.class);
+        Statement mockStatement = mock(Statement.class);
+        ResultSet mockResultSet = mock(ResultSet.class);
+
+        // Mocking query result
+        when(mockConnection.createStatement()).thenReturn(mockStatement);
+        when(mockStatement.executeQuery(anyString())).thenReturn(mockResultSet);
+        when(mockResultSet.next()).thenReturn(true, false);
+        when(mockResultSet.getString("CapitalCity")).thenReturn("Washington");
+        when(mockResultSet.getString("CountryName")).thenReturn("United States");
+        when(mockResultSet.getInt("CapitalPopulation")).thenReturn(572059);
+        when(mockResultSet.getString("Continent")).thenReturn("North America");
+        when(mockResultSet.getString("Region")).thenReturn("North America");
+
+        // Create the CountryLoader instance
+        CapitalLoader capitalLoader = new CapitalLoader();
+
+        // Call the method to be tested
+        List<Capital> capitals = CapitalLoader.loadCapitalData(mockConnection);
+
+        // Verify the result
+        assertEquals(1, capitals.size());
+        Capital capital = capitals.get(0);
+        assertEquals("Washington", capital.getName());
+        assertEquals("United States", capital.getCountry());
+        assertEquals(572059, capital.getPopulation());
+        assertEquals("North America", capital.getContinent());
+        assertEquals("North America", capital.getRegion());
+    }
+
+    @Test
+    void countryIsNull() {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        countries.add(null);
+        countryReport.sortByPopulation();
+    }
+
+    @Test
+    void countryIsEmpty()
+    {
+        ArrayList<Country> countries = new ArrayList<Country>();
+        countryReport.sortByPopulation();
+    }
+
     /**
      * Sort By Population in Descending
      */
     @Test
     void testSortByPopulation() {
         // Execute the method under test
+        ArrayList<Country> countries = new ArrayList<Country>();
         countryReport.sortByPopulation();
+
+        cityReport.sortByPopulation();
+        capitalReport.sortByPopulation();
     }
 
     /**
@@ -81,6 +195,8 @@ public class AppTest {
     void testSortByPopulationContinent() {
         // Execute the method under test
         countryReport.sortByPopulationContinent();
+        cityReport.sortByPopulationContinent();
+        capitalReport.sortByPopulationContinent();
     }
 
     /**
@@ -90,6 +206,18 @@ public class AppTest {
     void testSortByPopulationRegion() {
         // Execute the method under test
         countryReport.sortByPopulationRegion();
+        cityReport.sortByPopulationRegion();
+        capitalReport.sortByPopulationRegion();
+    }
+
+    @Test
+    void testSortByCountry() {
+        cityReport.sortByPopulationCountry();
+    }
+
+    @Test
+    void testSortByDistrict() {
+        cityReport.sortByPopulationDistrict();
     }
 
      /**
@@ -97,9 +225,9 @@ public class AppTest {
      */
     @Test
     void testTopNPopulatedCountriesInWorld() {
-
-        // Execute the method under test
         List<Country> topCountries = countryReport.getTopNPopulatedCountriesInWorld(3);
+        List<City> topCities = cityReport.getTopNPopulatedCitiesInWorld(4);
+        List<Capital> topCapitals = capitalReport.getTopNPopulatedCapitalsInWorld(3);
     }
 
     /**
@@ -107,9 +235,9 @@ public class AppTest {
      */
     @Test
     void testTopNPopulatedCountriesContinent() {
-
-        // Execute the method under test
         List<Country> topCountries = countryReport.getTopNPopulatedCountriesInContinent("Asia", 1);
+        List<City> topCities = cityReport.getTopNPopulatedCitiesInContinent("Asia",1);
+        List<Capital> topCapitals = capitalReport.getTopNPopulatedCapitalsInContinent("Asia", 1);
     }
 
     /**
@@ -117,7 +245,19 @@ public class AppTest {
      */
     @Test
     void testTopNPopulatedCountriesRegion() {
-        // Execute the method under test
         List<Country> topCountries = countryReport.getTopNPopulatedCountriesInRegion("Eastern Asia", 1);
+        List<City> topCities = cityReport.getTopNPopulatedCitiesInRegion("Eastern Asia", 1);
+        List<Capital> topCapitals = capitalReport.getTopNPopulatedCapitalsInRegion("Eastern Asia", 3);
     }
+
+    @BeforeEach
+    void setUp() {
+        mySQL = new MySQLSetUp();
+    }
+
+    @AfterEach
+    void tearDown() {
+        mySQL.disconnect();
+    }
+
 }
