0a626b38fcd24a524590063165e350f56ab0b694
Poe_2332 <40632015@live.napier.ac.uk>
2024-02-19 12:07:58 +0630

    Added total population report for each country

--- src/main/java/DevOps_Team8/PopulationReport.java
+++ src/main/java/DevOps_Team8/PopulationReport.java
@@ -76,5 +76,36 @@ public class PopulationReport {
             e.printStackTrace();
         }
     }
+
+    public void CountryPopulationReport() {
+        String countryQuery = "SELECT country.Name AS Name, " +
+                "SUM(country.Population) AS TotalPopulation, " +
+                "SUM(city.Population) AS PopulationInCities, " +
+                "(SUM(country.Population) - SUM(city.Population)) AS PopulationNotInCities " +
+                "FROM country " +
+                "LEFT JOIN city ON country.Code = city.CountryCode " +
+                "GROUP BY Name";
+
+        try (PreparedStatement statement = connection.prepareStatement(countryQuery)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String countryName = resultSet.getString("Name");
+                long totalPopulation = resultSet.getLong("TotalPopulation");
+                long populationInCities = resultSet.getLong("PopulationInCities");
+                long populationNotInCities = resultSet.getLong("PopulationNotInCities");
+
+                double populationInCitiesPercentage = ((double) populationInCities / totalPopulation) * 100;
+                double populationNotInCitiesPercentage = ((double) populationNotInCities / totalPopulation) * 100;
+
+                System.out.println("Country Name: " + countryName);
+                System.out.println("Total Population: " + numberFormat.format(totalPopulation));
+                System.out.println("Population in Cities: " + numberFormat.format(populationInCities) + " (" + populationInCitiesPercentage + "%)");
+                System.out.println("Population not in Cities: " + numberFormat.format(populationNotInCities) + " (" + populationNotInCitiesPercentage + "%)");
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
