6a2ed3d6d991ff415cccc126a85353c316604749
Poe_2332 <40632015@live.napier.ac.uk>
2024-02-19 12:06:54 +0630

    Added total population report for each continent

---
+++ src/main/java/DevOps_Team8/PopulationReport.java
@@ -0,0 +1,49 @@
+package DevOps_Team8;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.NumberFormat;
+
+public class PopulationReport {
+
+    private Connection connection;
+    NumberFormat numberFormat = NumberFormat.getInstance();
+
+    public PopulationReport(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void ContinentPopulationReport() {
+        String continentQuery = "SELECT Continent AS Name, " +
+                "SUM(country.Population) AS TotalPopulation, " +
+                "SUM(city.Population) AS PopulationInCities, " +
+                "(SUM(country.Population) - SUM(city.Population)) AS PopulationNotInCities " +
+                "FROM country " +
+                "LEFT JOIN city ON country.Code = city.CountryCode " +
+                "GROUP BY Continent";
+
+        try (PreparedStatement statement = connection.prepareStatement(continentQuery)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String continentName = resultSet.getString("Name");
+                long totalPopulation = resultSet.getLong("TotalPopulation");
+                long populationInCities = resultSet.getLong("PopulationInCities");
+                long populationNotInCities = resultSet.getLong("PopulationNotInCities");
+
+                double populationInCitiesPercentage = ((double) populationInCities / totalPopulation) * 100;
+                double populationNotInCitiesPercentage = ((double) populationNotInCities / totalPopulation) * 100;
+
+                System.out.println("Continent: " + continentName);
+                System.out.println("Total Population: " + numberFormat.format(totalPopulation));
+                System.out.println("Population in Cities: " + numberFormat.format(populationInCities) + " (" + populationInCitiesPercentage + "%)");
+                System.out.println("Population not in Cities: " + numberFormat.format(populationNotInCities) + " (" + populationNotInCitiesPercentage + "%)");
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
