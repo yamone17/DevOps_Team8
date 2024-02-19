a11691a4b19700c7000ac285d56c8c2e3fa041d9
Poe_2332 <40632015@live.napier.ac.uk>
2024-02-19 12:07:29 +0630

    Added total population report for each region

--- src/main/java/DevOps_Team8/PopulationReport.java
+++ src/main/java/DevOps_Team8/PopulationReport.java
@@ -45,5 +45,36 @@ public class PopulationReport {
             e.printStackTrace();
         }
     }
+
+    public void RegionPopulationReport() {
+        String regionQuery = "SELECT Region AS Name, " +
+                "SUM(country.Population) AS TotalPopulation, " +
+                "SUM(city.Population) AS PopulationInCities, " +
+                "(SUM(country.Population) - SUM(city.Population)) AS PopulationNotInCities " +
+                "FROM country " +
+                "LEFT JOIN city ON country.Code = city.CountryCode " +
+                "GROUP BY Region";
+
+        try (PreparedStatement statement = connection.prepareStatement(regionQuery)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String regionName = resultSet.getString("Name");
+                long totalPopulation = resultSet.getLong("TotalPopulation");
+                long populationInCities = resultSet.getLong("PopulationInCities");
+                long populationNotInCities = resultSet.getLong("PopulationNotInCities");
+
+                double populationInCitiesPercentage = ((double) populationInCities / totalPopulation) * 100;
+                double populationNotInCitiesPercentage = ((double) populationNotInCities / totalPopulation) * 100;
+
+                System.out.println("Region: " + regionName);
+                System.out.println("Total Population: " + numberFormat.format(totalPopulation));
+                System.out.println("Population in Cities: " + numberFormat.format(populationInCities) + " (" + populationInCitiesPercentage + "%)");
+                System.out.println("Population not in Cities: " + numberFormat.format(populationNotInCities) + " (" + populationNotInCitiesPercentage + "%)");
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
